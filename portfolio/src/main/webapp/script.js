c409ff53c3db8385248d459730aef541bf03635e
MagoDelBlocco <bgdnkln@gmail.com>
2020-07-20 11:54:34 +0000

    refactor js expand function name

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function expand_collapsible(element) {
-  var content = element.nextElementSibling;
+function expandCollapsible(element) {
+  const content = element.nextElementSibling;
 
   content.style.maxHeight = content.style.maxHeight ? null :
                                                       content.scrollHeight + 'px';
